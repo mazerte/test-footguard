6eb5d1bb304166bd63472f6e2a993bb45bf553ce
Mathieu Desvé <mathieu.desve@me.com>
2013-12-26 17:36:11 +0100

    Remove dev null re

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -16,8 +16,7 @@ message=$(git log -1 --pretty=%B)
 cd dist
 rm -rf .git
 git init
-yes | git remote add heroku "git@heroku.com:$APP.git" > /dev/null 2>&1
-git push origin --delete heroku > /dev/null 2>&1
+yes | git remote add heroku "git@heroku.com:$APP.git"
 git checkout -b heroku
 git add --all
 git commit -m "$message"
