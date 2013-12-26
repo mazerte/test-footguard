857137fa6e47a63ee82c7ead30de36c79f7b1466
Mathieu Desvé <mathieu.desve@me.com>
2013-12-26 14:34:19 +0100

    Re try

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -9,9 +9,9 @@ message=$(git log -1 --pretty=%B)
 cd dist
 rm -rf .git
 git init
-git remote add origin $REPO
-git push origin --delete gh-pages
+git remote add origin $REPO > /dev/null
+git push origin --delete gh-pages > /dev/null
 git checkout -b gh-pages
 git add --all
 git commit -m "$message"
-git push origin gh-pages
+git push origin gh-pages > /dev/null
