b21d6b314f37fe41b5748db1edc49aca2efbfd53
Mathieu Desvé <mathieu.desve@me.com>
2013-12-27 17:05:40 +0100

    Try to add ssh key

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -3,13 +3,10 @@
 # if [ -z "$TRAVIS" ]
 # then
 echo "Prepare heroku"
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 echo "Host heroku.com" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
-heroku keys:clear
-yes | heroku keys:add
 # fi
 
 message=$(git log -1 --pretty=%B)
