4102fa34060ba1822f7454365be74c149ebd7a54
Mathieu Desvé <mathieu.desve@me.com>
2013-12-27 22:04:19 +0100

    Try without pass

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -7,9 +7,10 @@ echo "Host heroku.com" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config
 echo "   IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-echo -n $id_rsa_{0..13} >> ~/.ssh/id_rsa_base64
+echo -n $id_rsa_{0..23} >> ~/.ssh/id_rsa_base64
 base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 # fi
 
 message=$(git log -1 --pretty=%B)
