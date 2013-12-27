8e4e5b9e8ffe8c0add187b53e1a410802a6192ae
Mathieu Desvé <mathieu.desve@me.com>
2013-12-27 21:18:31 +0100

    List file in ssh folder

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -10,13 +10,14 @@ echo "   IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
 echo -n $id_rsa_{0..23} >> ~/.ssh/id_rsa_base64
 base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-eval `ssh-agent -s`
-sudo apt-get install expect
-/usr/bin/expect <<EOD
-spawn ssh-add ~/.ssh/id_rsa
-expect "nter passphrase"
-send $SSH_PASS"/r"
-interact
+ls -Flash ~/.ssh/
+# eval `ssh-agent -s`
+# sudo apt-get install expect
+# /usr/bin/expect <<EOD
+# spawn ssh-add ~/.ssh/id_rsa
+# expect "nter passphrase"
+# send $SSH_PASS"/r"
+# interact
 EOD
 # fi
 
